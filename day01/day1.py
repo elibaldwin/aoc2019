7fe9822a9d4175ebb9e21c9ff61c53263be02c92
Elias Baldwin <eliasab@uw.edu>
2019-12-02 16:55:21 -0800

    added python solutions for days 1 and 2

---
+++ day01/day1.py
@@ -0,0 +1,17 @@
+def fuel_for(weight):
+  total = 0
+  while weight > 0:
+    fuel = (weight // 3) - 2
+    if (fuel > 0):
+      total += fuel
+    weight = fuel
+  return total
+
+with open("day01/input.txt") as f:
+  weights = [int(x) for x in f.readlines()]
+  
+  part1 = sum([((x // 3) - 2) for x in weights])
+  print ("Part 1: ", part1)
+
+  part2 = sum([fuel_for(x) for x in weights])
+  print ("Part 2: ", part2)
---
+++ day02/day2.py
@@ -0,0 +1,36 @@
+with open("day02/input.txt") as f:
+  code = tuple([int(x) for x in f.read().strip().split(",")])
+
+def run_code(code, noun, verb):
+  code = list(code)
+
+  code[1] = noun
+  code[2] = verb
+
+  pos = 0
+  opcode = code[pos]
+
+  while opcode != 99:
+    in1 = code[pos+1]
+    in2 = code[pos+2]
+    out = code[pos+3]
+
+    if opcode == 1:
+      code[out] = code[in1] + code[in2]
+    elif opcode == 2:
+      code[out] = code[in1] * code[in2]
+      
+    pos += 4
+    opcode = code[pos]
+  return code[0]
+
+def find_solution(code, goal):
+  for noun in range(100):
+    for verb in range(100):
+      output = run_code(code, noun, verb)
+      if output == goal:
+        return 100 * noun + verb
+
+print("Part 1: ", run_code(code, 12, 2))
+
+print("Part 2: ", find_solution(code, 19690720))
